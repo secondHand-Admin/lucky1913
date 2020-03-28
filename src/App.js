846671c0157a52a1d4fec7ee0b96a499e58c1a71
wei666aw <1819049569@qq.com>
2020-03-28 15:11:47 +0800

    登录页

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Admin from './pages/Admin/index';
-import {HashRouter,Route,Redirect} from 'react-router-dom';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
+import Login from './pages/Login/index'
 function App() {
   return (
     <div className="App">
-     <HashRouter>
-       <Route path="/admin" render={()=>{
-         return(
-          <Admin>
-        </Admin>
-         )
-       }}></Route>
-     </HashRouter>
+      <HashRouter>
+        <Route path='/login' component={Login}></Route>
+        <Route path="/admin" render={() => {
+          return (
+            <Admin>
+            </Admin>
+          )
+        }}></Route>
+      </HashRouter>
     </div>
   );
 }
