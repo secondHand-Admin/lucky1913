f2975aa19cb026bbfa164d49b0f273c093da8e11
小小 <835075911@qq.com>
2020-03-29 23:36:27 +0800

    redirect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Admin from './pages/Admin/index';
 import Login from './pages/Login/index'
 import Home from './pages/Home/index';
-import { HashRouter, Route,Redirect } from 'react-router-dom';
+import { HashRouter, Route,Redirect,Switch } from 'react-router-dom';
 import GoodsGM from './pages/Goods/GoodsGM/index'
 import GoodsLB from './pages/Goods/GoodsLB/index'
 import AdminiStratorList from './pages/AdminiStrator/AdminiStratorList/index'
@@ -12,21 +12,24 @@ function App() {
   return (
     <div className="App">
       <HashRouter>
-        <Route path='/login' component={Login}></Route>
-        <Redirect exact from='/admin' to='/admin/home'></Redirect>
+       
+       <Route path='/login' component={Login}></Route>
         <Route path="/admin" render={() => {
           return (
             <Admin>
-              <Route path='/admin/home' component={Home}></Route>
-              <Route path='/admin/goodsInfo' component={GoodsGM}></Route>
-              <Route path='/admin/goodsKind' component={GoodsLB}></Route>
-              <Route path='/admin/administrator/adminList' component={AdminiStratorList}></Route>
-              <Route path='/admin/administrator/adminAdd' component={AdminiStratorAdd}></Route>
-              <Route path='/admin/set' component={PersonalNav}>
-              </Route>
+              <Switch>
+                <Redirect exact from='/admin' to='/admin/home'></Redirect>
+                <Route path='/admin/home' component={Home}></Route>
+                <Route path='/admin/goodsInfo' component={GoodsGM}></Route>
+                <Route path='/admin/goodsKind' component={GoodsLB}></Route>
+                <Route path='/admin/administrator/adminList' component={AdminiStratorList}></Route>
+                <Route path='/admin/administrator/adminAdd' component={AdminiStratorAdd}></Route>
+                <Route path='/admin/set' component={PersonalNav}></Route>
+              </Switch>
             </Admin>
           )
         }}></Route>
+       
       </HashRouter>
     </div>
   );
